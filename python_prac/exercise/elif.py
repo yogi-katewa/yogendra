0eaf9867dad6c97cc86ee9f5045e98be778e02e6
yogi-katewa <yogikatewa@gmail.com>
2017-01-12 17:32:29 +0530

    Exercise upload successfully

---
+++ python_prac/exercise/elif.py
@@ -0,0 +1,10 @@
+x=int(input("Enter an intrger:"))
+if x<0:
+    x=0
+    print("negative value changed to zero")
+elif x==0:
+    print("zero")
+elif x==1:
+    print("one")
+else:
+    print("more than one")   
